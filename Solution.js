4f8ed0790b75724c005e0d8a96288fc594f43c70
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-22 21:59:38 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,41 @@
+
+ * @param {number} inputValue
+ * @return {number}
+ */
+var countPrimes = function (inputValue) {
+    this.NO_PRIMES_FOUND = 0;
+    return countPrimesWithSieveOfEratosthenes(inputValue);
+};
+
+/**
+ * @param {number} inputValue
+ * @return {number}
+ */
+function countPrimesWithSieveOfEratosthenes(inputValue) {
+    if (inputValue < 2) {
+        return this.NO_PRIMES_FOUND;
+    }
+
+    const FIRST_PRIME_NUMBER = 2;
+    const REDUCTION_FOR_NON_INCLUSIVE_RANGE = 1;
+
+    const visited = new Array(inputValue + 1).fill(false);
+    const maxFactor = Math.floor(Math.sqrt(inputValue));
+    let nonPrimeNumbers = FIRST_PRIME_NUMBER - 1;
+
+    for (let factor = FIRST_PRIME_NUMBER; factor <= maxFactor; ++factor) {
+        if (visited[factor]) {
+            continue;
+        }
+
+        let currentValue = factor * factor;
+        while (currentValue < inputValue) {
+            if (!visited[currentValue]) {
+                visited[currentValue] = true;
+                ++nonPrimeNumbers;
+            }
+            currentValue += factor;
+        }
+    }
+    return inputValue - nonPrimeNumbers - REDUCTION_FOR_NON_INCLUSIVE_RANGE;
+}
